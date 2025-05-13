5dd3f2164b693dbf7da95f56e28e794857ed2018
Federico <federico.vani@icloud.com>
2025-05-13 15:55:25 +0200

    no message

---
+++ src/main/java/it/uniroma3/siw/model/Sponsor.java
@@ -0,0 +1,15 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Sponsor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nome;
+    private String descrizione;
+}
