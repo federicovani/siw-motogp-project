b5bfe2bb0f0feb0dbd95cc298645df615e9e403a
Federico <federico.vani@icloud.com>
2025-05-13 15:46:52 +0200

    Classe Pilota e Team

--- src/main/java/it/uniroma3/siw/model/Pilota.java
+++ src/main/java/it/uniroma3/siw/model/Pilota.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
 public class Pilota {
     @Id
@@ -12,7 +14,73 @@ public class Pilota {
     private Long id;
     private String nome;
     private String cognome;
+    private int numeroIdentificativo;
+    private java.time.LocalDate dataDiNascita;
+    private String nazionalita;
+    private int peso;
+    private int altezza;
 
+    public int getAltezza() {
+        return altezza;
+    }
 
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
 
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getNazionalita() {
+        return nazionalita;
+    }
+
+    public void setNazionalita(String nazionalita) {
+        this.nazionalita = nazionalita;
+    }
+
+    public LocalDate getDataDiNascita() {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(LocalDate dataDiNascita) {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public int getNumeroIdentificativo() {
+        return numeroIdentificativo;
+    }
+
+    public void setNumeroIdentificativo(int numeroIdentificativo) {
+        this.numeroIdentificativo = numeroIdentificativo;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
---
+++ src/main/java/it/uniroma3/siw/model/Team.java
@@ -0,0 +1,48 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nome;
+    private List<Pilota> pilotiUfficiali;
+    private List<Pilota> pilotiTester;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Pilota> getPilotiUfficiali() {
+        return pilotiUfficiali;
+    }
+
+    public void setPilotiUfficiali(List<Pilota> pilotiUfficiali) {
+        this.pilotiUfficiali = pilotiUfficiali;
+    }
+
+    public List<Pilota> getPilotiTester() {
+        return pilotiTester;
+    }
+
+    public void setPilotiTester(List<Pilota> pilotiTester) {
+        this.pilotiTester = pilotiTester;
+    }
+}
+
