cd30a898c53c60dd0e7c72ce998251358519be1e
Federico <federico.vani@icloud.com>
2025-05-20 15:30:28 +0200

    dependencies security

---
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -0,0 +1,30 @@
+package it.uniroma3.siw.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+@Configuration
+@EnableWebSecurity
+public class AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .authoritiesByUsernameQuery("SELECT username, role from credentials WHERE username=?")
+                .usersByUsernameQuery("SELECT username, password, 1 as enabled FROM credentials WHERE username=?");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package it.uniroma3.siw.controller;
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.service.CredentialsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    private CredentialsService credentialsService;
+
+    @GetMapping(value = "/")
+    public String index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "index.html";
+        }
+        else {
+            UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+            if (credentials.getRole().equals(Credentials.ADMIN_ROLE)) {
+                return "admin/indexAdmin.html";
+            }
+        }
+        return "index.html";
+    }
+}
