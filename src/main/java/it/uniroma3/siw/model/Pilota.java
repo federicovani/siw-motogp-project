eaf723094c6c2e85e10354796920317b971892ad
Elisa Rubeo <rubeo.elisa4@gmail.com>
2025-05-20 13:51:29 +0200

    no message

--- src/main/java/it/uniroma3/siw/model/Pilota.java
+++ src/main/java/it/uniroma3/siw/model/Pilota.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 
 import java.time.LocalDate;
-import java.util.Map;
 
 @Entity
 public class Pilota {
