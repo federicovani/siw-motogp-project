e461217feee47ddf5cc9296210d383a9f426066c
Sara <sar.valletta@stud.uniroma3.it>
2025-06-23 11:16:32 +0200

    fix vari

--- src/main/java/it/uniroma3/siw/controller/AdminController.java
+++ src/main/java/it/uniroma3/siw/controller/AdminController.java
@@ -6,19 +6,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import it.uniroma3.siw.service.PilotaService;
+import it.uniroma3.siw.sessionData.SessionData;
 
 @Controller
 public class AdminController {
 	
+	@Autowired private SessionData sessionData;
 	@Autowired private PilotaService pilotaService;
 	
 	
-	@GetMapping("/admin/adminHomepage") 
+	@GetMapping("/adminHomepage") 
 	public String getAdminHomepage() {
 		return "admin/adminHomepage";
 	}
 	
-	@GetMapping("/admin/adminPilotiETeam")
+	@GetMapping("/adminPilotiETeam")
 	public String showAdminPilotiETeam(Model model) {
 		model.addAttribute("piloti", this.pilotaService.getAllPiloti());
 		return "admin/adminPilotiETeam.html";
